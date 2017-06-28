a13e090248c2c8105bbf94f784cd91aee4d320e4
tropotek <info@tropotek.com>
2017-06-28 10:28:49 +1000

    Minor Code Updates - 252s-dev.vet.unimelb.edu.au

---
+++ Tk/DataMap/Db/JsonArray.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tk\DataMap\Db;
+
+use Tk\DataMap\Map;
+
+/**
+ * Class String
+ *
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+class JsonArray extends Map
+{
+    /**
+     * Map an array column value to an object property value
+     *
+     * @param array $row
+     * @param string $columnName
+     * @return mixed|null
+     */
+    public function toPropertyValue($row, $columnName)
+    {
+        $value = parent::toPropertyValue($row, $columnName);
+        if ($value) {
+            $value = json_decode($value);
+        }
+        if (!$value) $value = array();  // Ensure an empty array is returned
+        if ($value === null) $value = null;
+        return $value;
+    }
+
+    /**
+     * Map an object property value to an array column value
+     *
+     * @param mixed $object
+     * @param string $propertyName
+     * @return string|null
+     */
+    public function toColumnValue($object, $propertyName)
+    {
+        $value = parent::toColumnValue($object, $propertyName);
+        if ($value) {
+            $value = json_encode($value);
+        }
+        return $value;
+    }
+    
+}
+
