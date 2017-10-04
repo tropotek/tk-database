3b2bfa3fc3c55fe7006f9f1d13152c9dd63bee03
mifsudm <mifsudm@unimelb.edu.au>
2017-10-04 14:38:40 +1100

    Minor Code Updates - 252s-dev

--- Tk/DataMap/Db/ArrayObject.php
+++ Tk/DataMap/Db/ArrayObject.php
@@ -1,5 +1,5 @@
 <?php
-namespace Tk\DataMap\Form;
+namespace Tk\DataMap\Db;
 
 use Tk\DataMap\Map;
 
@@ -10,7 +10,7 @@ use Tk\DataMap\Map;
  * @link http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
  */
-class Text extends Map
+class ArrayObject extends Map
 {
 
     /**
@@ -23,9 +23,7 @@ class Text extends Map
     public function toPropertyValue($row, $columnName)
     {
         $value = parent::toPropertyValue($row, $columnName);
-        if ($value !== null) {
-            $value .= '';
-        }
+        $value = explode(',', $value);
         return $value;
     }
 
@@ -39,9 +37,7 @@ class Text extends Map
     public function toColumnValue($object, $propertyName)
     {
         $value = parent::toColumnValue($object, $propertyName);
-        if ($value !== null) {
-            $value .= '';
-        }
+        $value = implode(',', $value);
         return $value;
     }
     
