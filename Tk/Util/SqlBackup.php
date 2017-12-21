538d634eac07322bfe10d24c89daff0f756cfca9
mifsudm <mifsudm@unimelb.edu.au>
2017-12-21 13:52:35 +1100

    ~Auto: Minor Code Updates - 252s-dev

--- Tk/Util/SqlBackup.php
+++ Tk/Util/SqlBackup.php
@@ -48,7 +48,7 @@ class SqlBackup
 
         $host = escapeshellarg($this->db->getOption('host'));
         $name = escapeshellarg($this->db->getOption('name'));
-        $user = escapeshellarg($this->db->getOption('user'));
+        $user = escapeshellarg($this->db->getOption('UserIface'));
         $pass = escapeshellarg($this->db->getOption('pass'));
 
         $command = '';
@@ -95,7 +95,7 @@ class SqlBackup
 
         $host = escapeshellarg($this->db->getOption('host'));
         $name = escapeshellarg($this->db->getOption('name'));
-        $user = escapeshellarg($this->db->getOption('user'));
+        $user = escapeshellarg($this->db->getOption('UserIface'));
         $pass = escapeshellarg($this->db->getOption('pass'));
 
         $command = '';
