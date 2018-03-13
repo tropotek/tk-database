055d9c7d083c0ea2de1542e843609bf39a60b033
mifsudm <mifsudm@unimelb.edu.au>
2018-03-14 07:43:38 +1100

    ~Auto: Added a db backup option to ems CLI

--- Tk/Util/SqlBackup.php
+++ Tk/Util/SqlBackup.php
@@ -35,10 +35,11 @@ class SqlBackup
      * Restore an sql file
      *
      * @param string $sqlFile
+     * @param array $options  TODO:
      * @throws Exception
      * @throws \Tk\Exception
      */
-    public function restore($sqlFile)
+    public function restore($sqlFile, $options = array())
     {
         //  $this->db->dropAllTables(true);     // TODO: leave this up to the user
         //  $this->db->exec(file_get_contents($sqlFile));           // Remove, no good for large backups....
@@ -74,11 +75,12 @@ class SqlBackup
      * The file name will be in the format of: {DbName}_2016-01-01-12-00-00.sql
      * if the path does not already contain a .sql file extension
      *
-     * @param $path
-     * @return bool|string Return the sql filename  on success false on fail
+     * @param string $path
+     * @param array $options  TODO:
+     * @return bool|string Return the sql filename on success false on fail
      * @throws Exception
      */
-    public function save($path)
+    public function save($path = '', $options = array())
     {
         $sqlFile = $path;
         if (!preg_match('/\.sql$/', $sqlFile)) {
@@ -119,8 +121,39 @@ class SqlBackup
         if(filesize($sqlFile) <= 0) {
             throw new \Tk\Db\Exception('Size of file '.$sqlFile.' is ' . filesize($sqlFile));
         }
-
         return $sqlFile;
+    }
+
+    /**
+     * @param array $options
+     * @return string   Return the sql dump generated
+     * @throws Exception
+     * @todo: See how this goes as it could possably have a memory issues with large databases, use SqlBackup:save() in those cases
+     */
+    public function dump($options = array())
+    {
+        $host = escapeshellarg($this->db->getOption('host'));
+        $name = escapeshellarg($this->db->getOption('name'));
+        $user = escapeshellarg($this->db->getOption('user'));
+        $pass = escapeshellarg($this->db->getOption('pass'));
+
+        $command = '';
+        // TODO: create a windows valid commands ????
+        if ('mysql' == $this->db->getDriver()) {
+            $command = sprintf('mysqldump --opt -h %s -u %s -p%s %s', $host, $user, $pass, $name);
+        } else if ('pgsql' == $this->db->getDriver()) {
+            $command = sprintf('export PGPASSWORD=%s && pg_dump --inserts -O -h %s -U %s %s', $pass, $host, $user, $name);
+        }
+        if(!$command) {
+            throw new \Tk\Db\Exception('Database driver not supported:  '.$this->db->getDriver());
+        }
+
+        exec($command, $out, $ret);
+
+        if ($ret != 0) {
+            throw new \Tk\Db\Exception(implode("\n", $out));
+        }
+        return implode("\n", $out);
     }
 
 }
