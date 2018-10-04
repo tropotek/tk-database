9b5cb40e56158ee89b8c212277e4196ae776f516
Tropotek <info@tropotek.com>
2018-10-05 07:19:14 +1000

    ~Auto: Commit

---
+++ Tk/DataMap/Form/Percent.php
@@ -0,0 +1,43 @@
+<?php
+namespace Tk\DataMap\Form;
+
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+class Percent extends Iface
+{
+
+    /**
+     * Map an array column value to an object property value
+     *
+     * @param array $row
+     * @param string $columnName
+     * @return float|null
+     */
+    public function toPropertyValue($row, $columnName)
+    {
+        $value = parent::toPropertyValue($row, $columnName);
+        if ($value !== null) $value = (float)($value/100);
+        return $value;
+    }
+
+    /**
+     * Map an object property value to an array column value
+     *
+     * @param mixed $object
+     * @param string $propertyName
+     * @return string|null
+     * @throws \ReflectionException
+     */
+    public function toColumnValue($object, $propertyName)
+    {
+        $value = parent::toColumnValue($object, $propertyName);
+        if ($value !== null) $value = ($value*100) . '';
+        return $value;
+    }
+    
+}
+
