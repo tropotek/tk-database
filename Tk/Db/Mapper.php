c86cf59ae244269ecaeee3cf4d9f82595955ecfc
mifsudm <mifsudm@unimelb.edu.au>
2020-02-05 11:56:34 +1100

    ~Auto: Commit

--- Tk/Db/Mapper.php
+++ Tk/Db/Mapper.php
@@ -242,15 +242,32 @@ abstract class Mapper extends \Tk\Db\Map\Mapper
      */
     public function makeMultiQuery($value, $columnName, $logic = 'OR', $compare = '=')
     {
+        return self::makeMultipleQuery($value, $columnName, $logic, $compare);
+    }
+
+
+    /**
+     * Create a sql query string from an array.
+     * Handy for testing multiple values
+     * EG:
+     *   "a.type = 'Self Assessment' AND a.type != 'Testing' AND 'Thinking'"
+     *
+     * @param array|mixed $value
+     * @param string $columnName
+     * @param string $logic   Logical Operator
+     * @param string $compare  Comparison Operator
+     * @return string
+     */
+    public static function makeMultipleQuery($value, $columnName, $logic = 'OR', $compare = '=')
+    {
         if (!is_array($value)) $value = array($value);
         $w = '';
         foreach ($value as $r) {
             if ($r === null || $r === '') continue;
-            $w .= sprintf('%s %s %s %s ', $columnName, $compare, $this->getDb()->quote($r), $logic);
+            $w .= sprintf('%s %s %s %s ', $columnName, $compare, \Tk\Config::getInstance()->getDb()->quote($r), $logic);
         }
         if ($w)
             $w = rtrim($w, ' '.$logic.' ');
         return $w;
     }
-
 }
